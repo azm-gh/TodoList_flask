6eb05471374ec214a5ba37ed3af1bd6e9344058e
Jakub <azm-gh@users.noreply.github.com>
2023-03-02 11:20:08 +0100

    Update program.py

--- program.py
+++ program.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, static_url_path="", static_folder="static")
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://jjehlik:tissuEa@DESKTOP-KS6CF9G/flask"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://userName:passwordGoesHere@DESKTOP-KS6CF9G/flask"
     #connect_string = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(DB_USER, DB_PASS, DB_HOST, DB_PORT, DATABASE)
 
     db.init_app(app)
